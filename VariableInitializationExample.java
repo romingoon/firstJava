4ae3120060561838509d352dce006424d0b590df
문승현 <romingoon@gmail.com>
2023-01-27 17:13:43 +0900

    Initial commit

---
+++ VariableInitializationExample.java
@@ -0,0 +1,10 @@
+package javaStudy;
+
+public class VariableInitializationExample {
+  public static void main(String[] args) {
+    int value=30;
+
+    int result = value + 10;
+    System.out.println(result);
+  }
+}
